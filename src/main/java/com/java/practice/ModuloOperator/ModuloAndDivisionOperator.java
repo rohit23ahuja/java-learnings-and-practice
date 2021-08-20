d9a404a6b79c7fc3173a4584f81369b3de7b0e15
rohitahuja <rohit23ahuja@gmail.com>
2021-08-21 01:01:29 +0530

    Configuring logback and slf4j.

--- src/main/java/com/java/practice/ModuloOperator/ModuloAndDivisionOperator.java
+++ src/main/java/com/java/practice/ModuloOperator/ModuloAndDivisionOperator.java
@@ -1,35 +1,39 @@
 package com.java.practice.ModuloOperator;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Division operator return quotient in division. Modulo operator returns
  * remainder of division.
  * 
  * @author rohitahuja
  *
- */
+ */ 
+@Slf4j
 public class ModuloAndDivisionOperator {
 	public static void main(String[] args) {
 		// division operator gives the quotient
-		System.out.println(11 / 4);
-		System.out.println(11 / 4.0);
+		
+		log.info("{}",11 / 4);
+		log.info("{}",11 / 4.0);
 
 		// modulo gives out the remainder
-		System.out.println(11 % 4);
+		log.info("{}",11 % 4);
 
 		// both division and quotient give exception when done by zero
-		// System.out.println(11/0);
-		// System.out.println(11%0);
+		// log.info("{}",11/0);
+		// log.info("{}",11%0);
 
 		// use modulo to determine number is odd or even
-		System.out.println(98 % 2 == 0);
-		System.out.println(97 % 2 == 1);
+		log.info("{}",98 % 2 == 0);
+		log.info("{}",97 % 2 == 1);
 
 		// In modulo operation
 		// when dividend < divisor. dividend is returned.
 		// when dividend == divisor zero
 		// dividend > divisor. remainder is returned
 		for (int j = 0; j < 100; j++) {
-			System.out.println(j % 5);
+			log.info("{}",j % 5);
 		}
 	}
 
