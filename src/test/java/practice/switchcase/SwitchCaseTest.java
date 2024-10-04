c21298302abea7beb740e9fa2b18b51e4a246dd0
Rohit Ahuja <rohit23ahuja@gmail.com>
2024-10-04 09:45:29 +0530

    switch case practice

---
+++ src/test/java/practice/switchcase/SwitchCaseTest.java
@@ -0,0 +1,66 @@
+package practice.switchcase;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class SwitchCaseTest {
+
+    @Test
+    public void whenValidValue1_thenReturnShortCode() {
+        SwitchCase s = new SwitchCase();
+        String i = "Petrol";
+        String o = s.getShortCode(i);
+        assertEquals("P", o);
+    }
+
+    @Test
+    public void whenValidValue2_thenReturnShortCode() {
+        SwitchCase s = new SwitchCase();
+        String i = "Diesel";
+        String o = s.getShortCode(i);
+        assertEquals("D", o);
+    }
+
+    @Test
+    public void whenArgumentInvalidValue_thenReturnShortCode() {
+        SwitchCase s = new SwitchCase();
+        String i = "rohit";
+        String o = s.getShortCode(i);
+        assertEquals("U", o);
+    }
+
+    @Test
+    public void whenArgumentNull_thenReturnShortCode() {
+        SwitchCase s = new SwitchCase();
+        String i = null;
+        String o = s.getShortCode(i);
+        assertEquals("U", o);
+    }
+
+    @Test
+    public void whenArgumentEmpty_thenReturnShortCode() {
+        SwitchCase s = new SwitchCase();
+        String i = "";
+        String o = s.getShortCode(i);
+        assertEquals("U", o);
+    }
+
+    @Test
+    public void whenArgumentNumber_thenReturnShortCode() {
+        SwitchCase s = new SwitchCase();
+        String i = "123";
+        String o = s.getShortCode(i);
+        assertEquals("U", o);
+    }
+
+    @Test
+    public void whenArgumentInvalidCase_thenReturnShortCode() {
+        SwitchCase s = new SwitchCase();
+        String i = "petRol";
+        String o = s.getShortCode(i);
+        assertEquals("U", o);
+    }
+
+}
